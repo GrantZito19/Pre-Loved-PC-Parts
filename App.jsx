a512843717f91675d46ae5c3466801cec212db6d
GrantZito19 <maykie619g@hotmail.co.za>
2025-05-12 20:59:18 +0200

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Sell from "./pages/Sell";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import PostPart from "./pages/PostPart";
@@ -14,6 +16,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/category/:type" element={<Category />} />
+          <Route path="/sell" element={<Sell />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/post" element={<PostPart />} />
